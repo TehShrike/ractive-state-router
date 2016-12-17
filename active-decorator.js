9ec53d36cee92052a818b0ee41feea5ff28cd6ba
Josh Duff <me@JoshDuff.com>
2016-12-16 22:40:16 -0600

    Putting the decorator in its own file to let others import it directly

---
+++ active-decorator.js
@@ -0,0 +1,22 @@
+module.exports = function makeStateIsActiveDecorator(stateRouter) {
+	return function activeDecorator(node, stateName, options, className = 'active') {
+		function applyCurrentState() {
+			if (stateRouter.stateIsActive(stateName, options)) {
+				node.classList.add(className)
+			} else {
+				node.classList.remove(className)
+			}
+		}
+
+		stateRouter.on('stateChangeEnd', applyCurrentState)
+
+		function teardown() {
+			stateRouter.removeListener('stateChangeEnd', applyCurrentState)
+			node.classList.remove(className)
+		}
+
+		return {
+			teardown
+		}
+	}
+}
--- render.js
+++ render.js
@@ -1,3 +1,5 @@
+const makeStateIsActiveDecorator = require('./active-decorator')
+
 const UPDATE_ROUTE_KEY = 'update_route'
 
 function wrapWackyPromise(promise, cb) {
@@ -110,29 +112,6 @@ function copy(value) {
 
 function object(o) {
 	return o && typeof o === 'object'
-}
-
-function makeStateIsActiveDecorator(stateRouter) {
-	return function activeDecorator(node, stateName, options, className = 'active') {
-		function applyCurrentState() {
-			if (stateRouter.stateIsActive(stateName, options)) {
-				node.classList.add(className)
-			} else {
-				node.classList.remove(className)
-			}
-		}
-
-		stateRouter.on('stateChangeEnd', applyCurrentState)
-
-		function teardown() {
-			stateRouter.removeListener('stateChangeEnd', applyCurrentState)
-			node.classList.remove(className)
-		}
-
-		return {
-			teardown
-		}
-	}
 }
 
 function isTemplate(inputTemplate) {
